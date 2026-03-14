92cebaf475a6e715cd51507da4274a2002fce8a4
azic <andrew.john.zic@gmail.com>
2026-03-15 00:22:00 +1100

    need to improve FLAG_OUTER if clause

--- pipelines/pipeline_10s.sh
+++ pipelines/pipeline_10s.sh
@@ -63,7 +63,7 @@ RUN_EXTRACT_DS="${RUN_EXTRACT_DS:-${SCRIPT_DIR}/scripts/slurm/run_dstools_extrac
 RUN_UNFLAG="${RUN_UNFLAG:-${SCRIPT_DIR}/scripts/slurm/run_unflag_beams.sh}"
 RUN_FLAG="${RUN_FLAG:-${SCRIPT_DIR}/scripts/slurm/run_flag.sh}"
 RUN_FLAGOUTER="${RUN_FLAGOUTER:-${SCRIPT_DIR}/scripts/slurm/run_flagouter_beams.sh}"
-FLAG_OUTER=${FLAG_OUTER:-"False"} # Whether to flag outer antennas to match the inner antennas used by CRACO
+FLAG_OUTER=${FLAG_OUTER:-0} # Whether to flag outer antennas to match the inner antennas used by CRACO
 
 # -------------------- Resources ------------------------
 ARRAY_SPEC="${ARRAY_SPEC:-0-35}"
@@ -198,7 +198,7 @@ jid_unflag=$( sbatch_submit "unflag_ms" "${UNFLAG_TIME}" "${FLAG_CPUS}" "${FLAG_
   SBID="${SBID}" DATA_ROOT="${DATA_ROOT}" PATTERN="${NATIVE10S_PATTERN}" CASA_SIF="${FLINT_CASA_SIF}" BIND_SRC="${BIND_SRC}" )
 jid_unflag=$(chain "$jid_unflag" "unflag_native")
 
-if $FLAG_OUTER == "True"; then
+if $FLAG_OUTER == "True"; then  #this should be "==0"
   jid_flagouter=$( sbatch_submit "flagouter" "${FLAG_TIME}" "${FLAG_CPUS}" "${FLAG_MEM}" "${ARRAY_SPEC}" "${RUN_FLAGOUTER}" "" \
     SBID="${SBID}" DATA_ROOT="${DATA_ROOT}" PATTERN="${NATIVE10S_PATTERN}" SCRIPT_DIR="${SCRIPT_DIR}" SCRIPT=${FLAGOUTER_SCRIPT} COLUMN="${FLAG_COLUMN}" )
   jid_flagouter=$(chain "$jid_flagouter" "flagouter_native")
